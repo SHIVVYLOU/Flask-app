72d4ceb6b1d4804fe926bf7d948ac1bde1e130b7
Ubuntu <ubuntu@ip-172-31-82-120.ec2.internal>
2019-07-23 11:13:40 +0000

    editted py

--- run.py
+++ run.py
@@ -2,33 +2,39 @@ import os
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, session
 
+
 app = Flask(__name__)
 app.secret_key = "randomstring123"
 messages = []
 
 
 def add_messages(username, message):
-    """Add messages to the messages list"""
-    now = datetime.now().strftime("%H:%M:$S")
-    messages.append("({}) {}: {}".format(now,username, message))
-    
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
+
+
 def get_all_messages():
-      return "<br>".join(messages)
+    """Get all of the messages and separate them with a `br`"""
+    return "<br>".join(messages)
 
 
-@app.route("/", methods = ["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    """ Main page with instructions """
+    """Main page with instructions"""
     if request.method == "POST":
         session["username"] = request.form["username"]
-        
-        if "username" in session:
-            return redirect (session ["username"])
-        return render_template("index.html")
-      
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
+
+
 @app.route("/<username>")
 def user(username):
-    return "<h1>Welcome {0}</h1>{1}".format(username, get_all_messages())
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 
 @app.route("/<username>/<message>")
